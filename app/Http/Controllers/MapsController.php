3e98ffc81cec663b9c23591903fb18bd3524a53e
nuaaaar <yanuar.fabien.yf@gmail.com>
2018-02-16 16:17:47 +0800

    Cek MapsController hiz

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -7,5 +7,6 @@ use Illuminate\Http\Request;
 
 class MapsController extends Controller
 {
-    //
+    // kalau status kunjungan towernya = belum dikunjungi . marker towernya merah
+    // kalau status kunjungan towernya = sudah dikunjungi . marker towernya hijau
 }
